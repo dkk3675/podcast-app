12b825b5290d4be12569c4fb86c022ebc897de86
Divya <karan.boss1000@gmail.com>
2023-04-24 14:07:18 +0530

    initial commit

--- login-and-register-backend/index.js
+++ login-and-register-backend/index.js
@@ -1,66 +1,111 @@
-import express from "express"
-import cors from "cors"
-import mongoose from "mongoose"
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/myLoginRegisterDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, () => {
-    console.log("DB connected")
-})
+const mongodbUri = `mongodb+srv://${process.env.MONGO_USER_ID}:${process.env.MONGO_PASSWORD}@cluster0.wkfdb3d.mongodb.net/login_register?retryWrites=true&w=majority`;
+
+mongoose.connect(mongodbUri, {
+  useNewUrlParser: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongodb...");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to mongo", err);
+});
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-})
+  name: String,
+  email: String,
+  password: String,
+  likedPodcasts: [{ podcastName: String, isLiked: Boolean }],
+});
 
-const User = new mongoose.model("User", userSchema)
+const User = new mongoose.model("User", userSchema);
 
 //Routes
-app.post("/login", (req, res)=> {
-    const { email, password} = req.body
-    User.findOne({ email: email}, (err, user) => {
-        if(user){
-            if(password === user.password ) {
-                res.send({message: "Login Successfull", user: user})
-            } else {
-                res.send({ message: "Password didn't match"})
-            }
-        } else {
-            res.send({message: "User not registered"})
-        }
-    })
-}) 
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      if (password === user.password) {
+        res.send({ message: "Login Successful", user: user });
+      } else {
+        res.send({ message: "Password didn't match" });
+      }
+    } else {
+      res.send({ message: "User not registered" });
+    }
+  });
+});
 
-app.post("/register", (req, res)=> {
-    const { name, email, password} = req.body
-    User.findOne({email: email}, (err, user) => {
-        if(user){
-            res.send({message: "User already registerd"})
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      res.send({ message: "User already registered" });
+    } else {
+      const user = new User({
+        name,
+        email,
+        password,
+      });
+      user.save((err) => {
+        if (err) {
+          res.send(err);
         } else {
-            const user = new User({
-                name,
-                email,
-                password
-            })
-            user.save(err => {
-                if(err) {
-                    res.send(err)
-                } else {
-                    res.send( { message: "Successfully Registered, Please login now." })
-                }
-            })
+          res.send({ message: "Successfully Registered, Please login now." });
         }
-    })
-    
-}) 
+      });
+    }
+  });
+});
 
-app.listen(9002,() => {
-    console.log("BE started at port 9002")
-})
+app.post("/button-click", async (req, res) => {
+  const { email, podcast_name, isLiked } = req.body;
+  const user = await User.findOne({ email });
+  const podcast = user.likedPodcasts.find(
+    (podcast) => podcast.podcastName === podcast_name
+  );
+
+  if (podcast) {
+    podcast.isLiked = isLiked;
+  } else {
+    user.likedPodcasts.push({ podcastName: podcast_name, isLiked });
+  }
+
+  user.save((err) => {
+    if (err) {
+      console.log(err);
+      //   res.send({ message: false });
+    } else {
+      res.send(isLiked);
+    }
+  });
+});
+
+app.post("/isLiked", async (req, res) => {
+  const { email, podcast_name } = req.body;
+  const user = await User.findOne({ email });
+  const podcast = user.likedPodcasts.find(
+    (podcast) => podcast.podcastName === podcast_name
+  );
+  if (podcast) {
+    res.send(podcast.isLiked);
+  } else {
+    res.send(false);
+  }
+});
+
+app.listen(9002, () => {
+  console.log("BE started at port 9002");
+});
